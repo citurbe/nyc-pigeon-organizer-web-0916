70e0166811f1daf4f2f63a80bbe40af9cd78dd54
Craig Iturbe <craigiturbe@Craigs-MacBook-Air.local>
2016-09-18 12:22:39 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  output_hash = {}
+  data[:gender][:male].each do |name|
+  	output_hash[name] = {gender: ["male"]}
+  	output_hash[name][:color] = []
+  end
+  data[:gender][:female].each do |name|
+  	output_hash[name] = {gender: ["female"]}
+  	output_hash[name][:color] = []
+  end
+
+  data[:color].each do |color_name, pigeon_array|
+  	pigeon_array.each do |pigeon_name|
+  		color_array = output_hash[pigeon_name][:color]
+  		color_array << color_name.to_s
+  		output_hash[pigeon_name][:color] = color_array
+  	end
+  end
+  data[:lives].each do |location, pigeon_array|
+  	pigeon_array.each do |pigeon_name|
+  		output_hash[pigeon_name][:lives] = [location]
+  	end
+
+  	
+end
+output_hash
 end
